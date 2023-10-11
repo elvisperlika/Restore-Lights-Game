dc84e3e5968da3378abe9f54909930cb938ff3d6
IGieckI <giacomo.antonelli3@gmail.com>
2023-10-11 20:28:29 +0200

    Improve game_system.h class

--- src/game_system.h
+++ src/game_system.h
@@ -13,3 +13,32 @@ enum GameState {
     NEWLEVEL,
     GAMEOVER
 };
+
+// Initial leds turning off time
+#define INITIAL_T2 3000;
+
+// Initial time Player have to finish the level on easiest difficulty
+#define INITIAL_T3 10000;
+
+// F factors for each difficulty
+extern const double DECRESE_RATES[];
+
+// Current Player level
+extern int currentLevel;
+
+// Current game difficulty
+extern int currentDifficulty;
+
+/**
+ * Calculate the value of T2 of the given level.
+ * @param level: current level.
+ * @param difficulty: current difficulty.
+*/
+int CalculateT2(int level, int difficulty);
+
+/**
+ * Calculate the value of T3 of the given level.
+ * @param level: current level.
+ * @param difficulty: current difficulty.
+*/
+int CalculateT3(int level, int difficulty);
