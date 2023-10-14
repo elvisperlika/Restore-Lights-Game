90a42aa37c45132f664aac4a8c90e3987e3c1008
IGieckI <giacomo.antonelli3@gmail.com>
2023-10-14 09:52:21 +0200

    Refactory sleep_mode_utility files

--- src/sleep_mode_utility.cpp
+++ src/sleep_mode_utility.cpp
@@ -2,22 +2,24 @@
 #include <avr/sleep.h>
 #include <avr/power.h>
 #include "sleep_mode_utility.h"
+#include "button_manager.h"
 
-const int buttons[] = {BUTTON1, BUTTON2, BUTTON3, BUTTON4};
-
+/// @brief Activate deep sleep mode and set the interrupt to wake up the system for each button
 void sleepNow() {
     set_sleep_mode(SLEEP_MODE_PWR_DOWN);
     sleep_enable();
+    
     for (int i = 0; i < getButtonsNumber(); i++) {
         attachInterrupt(digitalPinToInterrupt(buttons[i]), wakeUpNow, RISING);
-    }
+    }    
     
     sleep_mode();
-    sleep_disable();
-    
+
+    sleep_disable();    
     for (int i = 0; i < getButtonsNumber(); i++) {
         detachInterrupt(digitalPinToInterrupt(buttons[i]));
     }
 }
 
+/// @brief Empty function for wake up interrupt
 void wakeUpNow(){};
--- src/sleep_mode_utility.h
+++ src/sleep_mode_utility.h
@@ -1,17 +1,10 @@
 #ifndef __SLEEP_MODE_UTILITY_H__
 #define __SLEEP_MODE_UTILITY_H__
-#include "led_manager.h"
-#include "button_manager.h"
 
-/**
- * Activate deep sleep mode and set the interrupt 
- * to wake up the system for each button.
-*/
+/// @brief Activate deep sleep mode and set the interrupt to wake up the system for each button
 void sleepNow();
 
-/**
- * Empty function for wake up interrupt.
-*/
+/// @brief Empty function for wake up interrupt
 void wakeUpNow();
 
 #endif
