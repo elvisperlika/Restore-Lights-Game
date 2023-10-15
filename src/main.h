dc3eadd1211886865545481039481b581227f3b6
IGieckI <giacomo.antonelli3@gmail.com>
2023-10-15 20:23:44 +0200

    Remove an old sign from main.h

--- src/main.h
+++ src/main.h
@@ -1,7 +1,4 @@
 #include <Arduino.h>
 
-/// @brief This function is a trampoline to the sleepNow() function.
-void sleepNowTrampoline();
-
 /// @brief Set the game state to GAMEOVER (used with an iterrupt).
 void setGameOver();
