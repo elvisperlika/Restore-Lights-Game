9e0cdec821c4f74b2473c0bd93dc3da620d8e301
IGieckI <giacomo.antonelli3@gmail.com>
2023-10-15 11:42:39 +0200

    Rename sleep_mode_utility to time_utility, move timer managers to time_utility file

---
+++ src/time_utility.cpp
@@ -0,0 +1,48 @@
+#include <Arduino.h>
+#include <avr/sleep.h>
+#include <avr/power.h>
+#include "time_utility.h"
+#include "button_manager.h"
+
+/// @brief Activate deep sleep mode and set the interrupt to wake up the system for each button
+void sleepNow() {
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+    sleep_enable();
+    
+    for (int i = 0; i < getButtonsNumber(); i++) {
+        attachInterrupt(digitalPinToInterrupt(buttons[i]), wakeUpNow, RISING);
+    }    
+    
+    sleep_mode();
+
+    sleep_disable();    
+    for (int i = 0; i < getButtonsNumber(); i++) {
+        detachInterrupt(digitalPinToInterrupt(buttons[i]));
+    }
+}
+
+/// @brief Empty function for wake up interrupt
+void wakeUpNow(){};
+
+/// @brief Call a function if enough time passed
+/// @param limitTime is the time after which the function is launched
+/// @param startTime is a pointer to a variable representing the start time
+/// @param function function to launch
+void basicTimer(unsigned long limitTime, unsigned long *startTime, void (*function)()) {
+    if (millis() - *startTime >= limitTime) {
+        function();
+        *startTime = millis();
+    }
+}
+
+/// @brief Call a function if enough time passed
+/// @param limitTime is the time after which the function is launched
+/// @param startTime is a pointer to a variable representing the start time
+/// @param function function to launch
+/// @param s is the function's parameter
+void basicTimer(unsigned long limitTime, unsigned long *startTime, void (*function)(bool), bool s) {
+    if (millis() - *startTime >= limitTime) {
+        function(s);
+        *startTime = millis();
+    }
+}
